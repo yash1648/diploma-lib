d32feb59b110848f14cb7efc1178324e6a343a7d
yash1648 <yashbagal623@gmail.com>
2024-12-09 16:53:05 +0000

    Modifying backend and requirements

--- backend/main.py
+++ backend/main.py
@@ -1,25 +1,20 @@
 from fastapi import FastAPI
 from pydantic import BaseModel
 from fastapi.middleware.cors import CORSMiddleware
+
 app = FastAPI()
+
+origins=[
+    "http://localhost:5173",
+    "http://localhost:5174"
+]
+
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5174","http://localhost:5174"], 
+    allow_origins=origins, 
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
 )
 
-class Item(BaseModel):
-    name: str
-    description: str
-    price: float
-
-@app.get("/")
-async def read_root():
-    return {"message": "Welcome to FastAPI"}
-
-@app.post("/items/")
-async def create_item(item: Item):
-    return {"message": f"Item {item.name} created", "data": item}
